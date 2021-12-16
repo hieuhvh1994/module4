9b5495982511c20ad154c3dbdeccbbb8bd0d7ccf
hieuhvh1994 <hieudaohn94@gmail.com>
2021-12-16 13:49:34 +0700

    Added SpringBootJSONWebToken

---
+++ module4/SpringBootJSONWebToken/src/main/java/com/example/springbootjsonwebtoken/SpringBootJsonWebTokenApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjsonwebtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJsonWebTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJsonWebTokenApplication.class, args);
+    }
+
+}
---
+++ module4/SpringBootJSONWebToken/src/test/java/com/example/springbootjsonwebtoken/SpringBootJsonWebTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjsonwebtoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJsonWebTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
